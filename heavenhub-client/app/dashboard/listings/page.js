10c73a480146ab16bf16592caff3442bd77f36bb
Abhik Nayak <abhiknayakabhik@gmail.com>
2025-04-24 07:57:37 +0530

    error fix

---
+++ heavenhub-client/app/dashboard/listings/page.js
@@ -0,0 +1,10 @@
+// app/protected/listings/page.js
+export default function ListingsPage() {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold mb-4">Listings</h1>
+        <p>All your listings appear here.</p>
+      </div>
+    );
+  }
+  
