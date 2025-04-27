f78df364628d2dfc8653f9d7f341f53c64aeb35f
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-04-27 17:15:15 +0530

    error fix

--- heavenhub-client/redux/store/index.js
+++ heavenhub-client/redux/store/index.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../slices/authSlice';
-import localStorageReducer from "../slices/localStorageslice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    storage: localStorageReducer
   },
 });
 
