d3e3fade11fdef08817b115f27c1b309906b8f6f
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-09-03 12:11:19 +0530

    Add Swagger API docs to user service
    Integrated swagger-jsdoc and swagger-ui-express for OpenAPI documentation in the user service. Added OpenAPI annotations to user routes for register, login, logout, get user, and update user endpoints. Updated package.json with Swagger dependencies.

--- services/user-service/app.js
+++ services/user-service/app.js
@@ -1,6 +1,8 @@
 import cookieParser from "cookie-parser";
 import {configDotenv } from "dotenv";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
 import connect from "./db/db.js";
 import userRoute from "./routes/user.route.js";
 
@@ -15,4 +17,30 @@ app.use(cookieParser());
 
 app.use('/api/user', userRoute);
 
-export default app;     
+// Swagger/OpenAPI setup
+const swaggerSpec = swaggerJsdoc({
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'User Service API',
+            version: '1.0.0'
+        },
+        servers: [
+            { url: 'http://localhost:3001' }
+        ],
+        components: {
+            securitySchemes: {
+                cookieAuth: {
+                    type: 'apiKey',
+                    in: 'cookie',
+                    name: 'token'
+                }
+            }
+        }
+    },
+    apis: ['./routes/*.js']
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+export default app; 
