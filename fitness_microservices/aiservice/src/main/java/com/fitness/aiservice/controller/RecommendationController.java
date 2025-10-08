3a2bef1b7f37f4bfbe078f34f791b4955f746021
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-10-08 08:45:07 +0530

    Add Activity model and optimize imports in controller
    Introduced the Activity model to represent user activities with relevant fields. Also reorganized imports in RecommendationController for better readability.

--- fitness_microservices/aiservice/src/main/java/com/fitness/aiservice/controller/RecommendationController.java
+++ fitness_microservices/aiservice/src/main/java/com/fitness/aiservice/controller/RecommendationController.java
@@ -1,15 +1,17 @@
 package com.fitness.aiservice.controller;
 
-import com.fitness.aiservice.model.Recommendation;
-import com.fitness.aiservice.service.RecommendationService;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.service.RecommendationService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
